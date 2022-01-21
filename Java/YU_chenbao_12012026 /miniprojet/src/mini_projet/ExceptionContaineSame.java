2344e291ec5e428b2a52d195cf80cf378690e60c
Chenbao2021 <93772528+Chenbao2021@users.noreply.github.com>
2022-01-21 11:11:23 +0100

    Delete Java directory

--- Java/YU_chenbao_12012026 /miniprojet/src/mini_projet/ExceptionContaineSame.java
+++
@@ -1,8 +0,0 @@
-package mini_projet;
-
-public class ExceptionContaineSame extends Exception {
-
-	public ExceptionContaineSame() {
-		super("Cette piece existe deja!");
-	}
-}
--- Java/YU_chenbao_12012026 /miniprojet/src/mini_projet/Piece_fragile.java
+++
@@ -1,5 +0,0 @@
-package mini_projet;
-
-public interface Piece_fragile {
-	
-}
